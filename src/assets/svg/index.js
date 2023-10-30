570bc5b8ad4f71bb406953136b34b1726428d2a5
Nalin Sakthivel <nalin.s@technotackle.com>
2023-10-31 00:05:15 +0530

    assets added

---
+++ src/assets/svg/index.js
@@ -0,0 +1,8 @@
+export {default as Info} from './Info.svg';
+export {default as Clock} from './Clock.svg';
+export {default as Coin} from './Coin.svg';
+export {default as Eclipse} from './EclipseBG.svg';
+export {default as Bitcoin} from './BitCoin.svg';
+export {default as DownArrow} from './UpArrow.svg';
+export {default as UpArrow} from './DownArrow.svg';
+export {default as User} from './User.svg';
---
+++ src/values/MyColor.ts
@@ -0,0 +1,13 @@
+export const MyColors = {
+  primary: '#6231AD',
+  button: '#452C55',
+  progressFailure: '#FE4190',
+  progressSuccess: '#2DABAD',
+  white: '#ffffff',
+  black: '#000000',
+  lightPrimary: '#D2BAF5',
+  lightGrey: '#F6F3FA',
+  mildBlue: '#B5C0C8',
+  secondary: '#727682',
+  teritaryText: '#B5C0C8',
+};
